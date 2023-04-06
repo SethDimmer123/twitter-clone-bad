566d78f4575c6c0f62ff51d0db372b3b9d8f7e10
Seth DImmer <sethdimmer1@gmail.com>
2023-04-06 19:44:35 -0400

    initial commit

---
+++ components/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Feed() {
+    //   flex-grow lets me take as much space as possible for middle(feed component)
+  return (
+  <div className='text-white flex-grow border-l border-r
+  border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>Feed
+  <div className='text-[#d9d9d9] flex items-center
+  sm:justify-between py-2 px-2 sticky'>
+
+    </div>
+  </div>
+  );
+}
+
+export default Feed
