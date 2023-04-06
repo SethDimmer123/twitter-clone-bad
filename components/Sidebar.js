9a486427ff9d865501457b2b9400aa3c1c3384d0
Seth DImmer <sethdimmer1@gmail.com>
2023-04-06 14:47:42 -0400

    initial commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,17 @@
 import Image from 'next/image'
-import React from 'react'
+import {HomeIcon} from "@heroicons/react/solid";
+import {
+    HashtagIcon,
+    BellIcon,
+    InboxIcon,
+    BookmarkIcon,
+    ClipboardListIcon,
+    UserIcon,
+    DotsCircleHorizontalIcon,
+    DotsHorizontalIcon,
+  } from "@heroicons/react/outline";
+import SidebarLink from './SidebarLink'
+
 
 function Sidebar() {
   return (
@@ -8,6 +20,16 @@ function Sidebar() {
         <div className='flex items-center justify-center w-14 h-14 
         hoverAnimation p-0 xl:ml-24'>
             <Image src="/twitter-logo-5.png" width={30} height={30}/>
+        </div>
+        <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
         </div>
     </div>
   )
