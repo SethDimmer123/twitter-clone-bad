a71eee9b295b26413eb5ef0f889dae03b58143a6
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-11 08:28:32 -0400

    initial commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,9 +11,11 @@ import {
     DotsHorizontalIcon,
   } from "@heroicons/react/outline";
 import SidebarLink from './SidebarLink'
+import { useSession } from 'next-auth/react';
 
 
 function Sidebar() {
+  const {data: session} = useSession();
   return (
     <div className='hidden sm:flex flex-col items-center xl:items-start
     xl:w-[340px] p-2 fixed h-full'>
@@ -39,12 +41,12 @@ function Sidebar() {
         </button>
         <div className='text-[#d9d9d9] flex items-center
         justify-center hoverAnimation xl:ml-auto xl:-mr-5 mt-auto'>
-            <img src="/unnamed.png" alt=''
+            <img src={session.user.image} alt=''
             className='h-10 w-10 rounded-full xl:mr-2.5'
             />
             <div className='hidden xl:inline leading-5'>
-                <h4 className='font-bold'>Seth Dimmer</h4>
-                <p className='text-[#6e767d]'>@lessgo2000</p>
+                <h4 className='font-bold'>{session.user.name}</h4>
+                <p className='text-[#6e767d]'>@{session.user.tag}</p>
             </div>
             <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" 
             />
