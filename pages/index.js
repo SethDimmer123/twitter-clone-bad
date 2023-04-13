fc8d55585efe92c66e10bb4bde388850a4c65886
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-13 12:07:40 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import { getProviders, getSession, useSession } from "next-auth/react";
 import Login from '../components/Login';
+import Modal from '../components/Modal';
 
 export default function Home({ trendingResults, followResults, providers }) {
   const {data: session} = useSession();
@@ -24,6 +25,7 @@ export default function Home({ trendingResults, followResults, providers }) {
         {/* Widgets */}
 
         {/* Modal */}
+        <Modal/>
       </main>
       </div>
   )
